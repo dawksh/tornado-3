aa8cad97144c750bf5c82e0a356051e7e00cbff6
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-03-06 00:40:00 +0530

    intit

---
+++ src/T3.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract T3 {
+    function generateVanityAddress(address sender, uint256 value) view {
+        // logic to generate create3 address
+    }
+}
