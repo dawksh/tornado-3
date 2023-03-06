4b872f73ab1634085af2cd9bb7dc2c7237b2cfe0
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-03-06 22:12:26 +0530

    feat: add deployment function

--- src/T3.sol
+++ src/T3.sol
@@ -18,4 +18,21 @@ contract T3 {
 
         return address(uint160(uint256(hash)));
     }
+
+    function deploy(bytes memory bytecode, uint256 _salt) public payable {
+        address addr;
+
+        assembly {
+            addr := create2(
+                callvalue(),
+                add(bytecode, 0x20),
+                mload(bytecode),
+                _salt
+            )
+
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+    }
 }
