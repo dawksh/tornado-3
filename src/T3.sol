290eded5e2f2c4858d3dafde99c2b238cb892880
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-03-06 22:09:32 +0530

    feat: add vanity address generation

--- src/T3.sol
+++ src/T3.sol
@@ -2,7 +2,20 @@
 pragma solidity ^0.8.13;
 
 contract T3 {
-    function generateVanityAddress(address sender, uint256 salt) view {
-        // logic to generate create3 address
+    function getAddress(bytes memory bytecode, uint256 _salt)
+        public
+        view
+        returns (address)
+    {
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                _salt,
+                keccak256(bytecode)
+            )
+        );
+
+        return address(uint160(uint256(hash)));
     }
 }
