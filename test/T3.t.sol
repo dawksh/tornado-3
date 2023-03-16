8f2205e7d8fd3f5cced871d3bb31558df2889668
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-03-16 22:02:22 +0530

    feat: add tests for sending funds

--- test/T3.t.sol
+++ test/T3.t.sol
@@ -8,25 +8,29 @@ import "../src/T3.sol";
 contract CounterTest is Test {
     T3 public t3;
 
+    bytes deploymentCode =
+        abi.encodePacked(type(T3Init).creationCode, abi.encode(address(this)));
+
     function setUp() external {
         t3 = new T3();
     }
 
     function testAddressComputation() external view {
-        bytes memory deploymentCode = abi.encodePacked(
-            type(T3Init).creationCode,
-            abi.encode(address(this))
-        );
         address deploymentAddress = t3.getAddress(deploymentCode, 269);
         assert(deploymentAddress != address(0));
     }
 
     function testAddressDeployment() external {
-        bytes memory deploymentCode = abi.encodePacked(
-            type(T3Init).creationCode,
-            address(this)
-        );
         address deploymentAddress = t3.getAddress(deploymentCode, 269);
-        assert(deploymentAddress != t3.deploy(269));
+        assertEq(deploymentAddress, t3.deploy(269));
+    }
+
+    function testSendingFunds() external {
+        address preCompute = t3.getAddress(deploymentCode, 269);
+        (bool s, ) = address(preCompute).call{value: 1 ether}("");
+        require(s);
+        uint256 bal = address(this).balance;
+        t3.deploy(269);
+        assertEq(bal + 1 ether, address(this).balance);
     }
 }
